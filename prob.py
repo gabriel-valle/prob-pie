d6d656e040e149072d7b6627805a127f72aae12b
Unknown <gabrielmvalle@gmail.com>
2019-03-31 13:48:15 -0300

    Define experiment class
    Allow experiment classes to be created from a sample space drawer

--- prob.py
+++ prob.py
@@ -1,8 +1,15 @@
 from collections import Counter
 
 def experiment(event, sample_draw, n=10000):
-    draws = []
-    for _ in range(n):
-        draws.append(sample_draw())
-    results = list(map(event, draws))
-    return results.count(True)/n
+
+class Experiment:
+    def __init__(self, result_drawer):
+        self.sample_draw = result_drawer
+        self.events = {}
+
+    def estimate(self, event, n=10000):
+        draws = []
+        for _ in range(n):
+            draws.append(self.sample_draw())
+        results = list(map(event, draws))
+        return results.count(True)/n
