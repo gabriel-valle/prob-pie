e93790bf5304b39269551faa93913395dacbeaeb
gabriel-valle <gabrielmvalle@gmail.com>
2019-04-02 10:08:57 -0300

    Define factorial, permutation and choosing operations

--- prob.py
+++ prob.py
@@ -1,5 +1,17 @@
 from collections import Counter
 
+def fact(n):
+    res = 1
+    for i in range(1,n+1):
+        res *= i
+    return res
+
+def perm(n, k):
+    return fact(n)/fact(n-k)
+
+def choose(n,k):
+    return fact(n)/(fact(k)*fact(n-k))
+
 def union(*events):
     return lambda result: any([ev(result) for ev in events])
     
